4de62fe64df64f599698fe17e0017cac658fe4b8
Alejandroem <Alex5652>
2015-10-05 23:39:14 -0600

    Este commit agrega la prueba del modelo y la funcionalidad del modelo para la aplicaciòn

--- screenjunkies/models.py
+++ screenjunkies/models.py
@@ -6,9 +6,9 @@ class Usuario(models.Model):
     Nombre = models.CharField(max_length=200, validators=[RegexValidator(r'^[a-zA-Z\' \']*$', 'El nombre solo permite letras de A-Z')])
     telefono = models.IntegerField(validators=[MaxValueValidator(99999999),MinValueValidator(10000000)])
     correoElectronico = models.EmailField()
-    fechaNacimiento = models.DateField()
+    fechaNacimiento = models.DateTimeField(auto_now_add=True)
     direccionDomicilio = models.CharField(max_length=200, validators=[RegexValidator(r'^[0-9a-zA-Z\'\-\'\' \']*$', 'La dirreccion no permite simbolos extranios')])
-    fechaInicioSocio = models.DateField()
+    fechaInicioSocio = models.DateTimeField(auto_now_add=True)
 
     def __unicode__(self):
         return self.Nombre
--- screenjunkies/tests.py
+++ screenjunkies/tests.py
@@ -1,30 +1,31 @@
 from django.test import TestCase
 from django.utils import timezone
 from screenjunkies.models import *
+
+
 # Create your tests here.
 class screenjunkiesViewsTestCase(TestCase):
     def test_index(self):
         resp = self.client.get('/registrarse/')
-        self.assertEqual(resp.status_code,200)
+        self.assertEqual(resp.status_code, 200)
 
 
-def screenjunkiesModelUsuarioTestCase(self):
+class screenjunkiesModelUsuarioTestCase(TestCase):
     def crear_usuario(self, nombre="Alejandro"
                       , telefono="55555555"
-                      , correoElectronico ="alexalejandro123@gmail.com"
-                      ,fechaNacimiento=timezone.now()
+                      , correoElectronico="alexalejandro123@gmail.com"
+                      , fechaNacimiento=timezone.now()
                       , direccionDomicilio="7av 16-16 A Zona 15"
-                      , fechaInicioSocio=timezone.now()):
-
-        return Usuario.objects.create(nombre=nombre
-                                      , telefono = telefono
+                      , fechaInicioSocio=timezone.now()
+                      ):
+        return Usuario.objects.create(Nombre=nombre
+                                      , telefono=telefono
                                       , correoElectronico=correoElectronico
-                                      ,fechaNacimiento=fechaNacimiento
-                                      ,direccionDomicilio=direccionDomicilio
-                                      ,fechaInicioSocio=fechaInicioSocio)
+                                      , fechaNacimiento=fechaNacimiento
+                                      , direccionDomicilio=direccionDomicilio
+                                      , fechaInicioSocio=fechaInicioSocio)
 
-    def prueba_crear_usuario(self):
+    def test_crear_usuario(self):
         u = self.crear_usuario()
         self.assertTrue(isinstance(u, Usuario))
-        self.assertEqual(u.__unicode__(), u.nombre)
-
+        self.assertEqual(u.__unicode__(), u.Nombre)
